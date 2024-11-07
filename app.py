28cae67228203259343dcb18f90326c6968611a3
Rafael <rafael.mattos468@gmail.com>
2024-11-07 14:05:25 -0300

    update

--- app.py
+++ app.py
@@ -7,19 +7,20 @@ import pywhatkit
 # webbrowser.open('https://web.whatsapp.com/')
 #sleep(5)
 #ler planilha e gaurdar informações sobre o nome, telefone e data de vencimento
-workbook = openpyxl.load_workbook('./clientes.xlsx')
-pagina_clientes = workbook['Sheet1']
+workbook = openpyxl.load_workbook('./alunos.xlsx')
+pagina_clientes = workbook['Planilha1']
 
 
 
 for linha in pagina_clientes.iter_rows(min_row=2):
-    sleep_time = random.uniform(1, 30) #tempo de espera entre cada mensagem
+    sleep_time = random.uniform(1, 20) #tempo de espera entre cada mensagem
     
     #nome, telefone, vencimento
     nome = linha[0].value
     telefone = str(linha[1].value)
     curso = linha[2].value
-    midia = './img/outubro rosa fpbe gestão.jpeg'
+    midia = './img/outuvembro.jpeg'
+    
     
     mensagem = f'''
 Olá {nome}, Gostaríamos de informar que o curso de *{curso}* está com as inscrições abertas. Para mais informações, entre em contato conosco.
@@ -28,7 +29,7 @@ Olá {nome}, Gostaríamos de informar que o curso de *{curso}* está com as insc
     '''
 
     #Preciso inserir a imagem aqui para ir junto com a mensagem
-    pywhatkit.sendwhats_image(telefone, midia, mensagem)
+    pywhatkit.sendwhats_image(telefone,midia, mensagem)
     
     sleep(5)
     pyautogui.hotkey('ctrl', 'w')  # Atalho para fechar a aba
